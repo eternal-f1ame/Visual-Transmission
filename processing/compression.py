cdb6e851da31b4d7d0412626f5c3c8061222e0b7
eternal-f1ame <thunder.tanay.1907@gmail.com>
2023-04-25 00:49:20 +0530

    Added send and receive function with serial communication

--- processing/compression.py
+++ processing/compression.py
@@ -9,13 +9,18 @@ with open('char_map.json', 'r') as f:
 char_map = {int(k): v for k, v in char_map.items()}
 reverse_char_map = {v: k for k, v in char_map.items()}
 
+huffman_codes = json.load(open('huffman_codes.json', 'r'))
+
 class TransmissionData():
-    def __init__(self, img):
+    def __init__(self, img, huffman_codes):
         self.img = img
         self.information = self.make_string_from_image()
-        self.string = r'<l>'+str(img.shape[0])+r'<h>'+str(img.shape[1])+r'<s>'+self.make_string_from_image()+r'<e>'
-        self.encoded_string, self.huffman_codes = self.huffman_encode()
+        self.string = '<l>'+str(img.shape[0])+'<h>'+str(img.shape[1])+'<s>'+self.make_string_from_image()+'<e>'
+        self.huffman_codes = huffman_codes
+        self.encoded_string = self.huffman_encode()
         self.encoded_string_length = len(self.encoded_string)
+
+        self.send()
 
     def make_string_from_image(self):
         image = self.img
@@ -23,12 +28,12 @@ class TransmissionData():
         for i in range(image.shape[0]):
             for j in range(image.shape[1]):
                 string.append(char_map[image[i][j]])
-        return r''.join(string)
+        return ''.join(string)
     
     def huffman_encode(self):
         text = self.string
         freq = collections.Counter(text)
-        heap = [[weight, [char, r'']] for char, weight in freq.items()]
+        heap = [[weight, [char, '']] for char, weight in freq.items()]
         heapq.heapify(heap)
         while len(heap) > 1:
             lo = heapq.heappop(heap)
@@ -39,9 +44,50 @@ class TransmissionData():
                 pair[1] = '1' + pair[1]
             heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
 
-        huffman_codes = dict(sorted(heapq.heappop(heap)[1:], key=lambda x: (len(x[-1]), x)))
-        return ''.join(huffman_codes[char] for char in text), huffman_codes
+        return ''.join(self.huffman_codes[char] for char in text)
 
     def show_image(self):
         plt.imshow(self.img, cmap='gray')
         plt.show()
+
+    def send(self):
+        ser = serial.Serial("/dev/tty.usbmodem11101", baudrate = 9600, timeout=3)
+        for _ in range(2):
+            for i in range(len(self.encoded_string)):
+                ser.write(bytearray(self.encoded_string[i],'ascii'))
+                print(i,ser.readline().decode('ascii'))
+
+    def gen_huffman_codes(images_dir):
+        all_strings = []
+        for image in os.listdir(images_dir):
+            string = []
+            img = PIL.Image.open(os.path.join(images_dir, image)).convert('L')
+            img = np.array(img).astype(np.uint8)
+            img = img//4
+            for i in range(img.shape[0]):
+                for j in range(img.shape[1]):
+                    string.append(char_map[img[i][j]])
+    
+            string = '<l>'+str(img.shape[0])+'<h>'+str(img.shape[1])+'<s>'+''.join(string)+'<e>'
+            all_strings.append(string)
+
+        text = ''.join(all_strings)
+        freq = collections.Counter(text)
+        heap = [[weight, [char, '']] for char, weight in freq.items()]
+        heapq.heapify(heap)
+        while len(heap) > 1:
+            lo = heapq.heappop(heap)
+            hi = heapq.heappop(heap)
+            for pair in lo[1:]:
+                pair[1] = '0' + pair[1]
+            for pair in hi[1:]:
+                pair[1] = '1' + pair[1]
+            heapq.heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+        huffman_codes = dict(heapq.heappop(heap)[1:])
+        with open('huffman_codes.json', 'w') as f:
+            json.dump(huffman_codes, f)
+
+img = np.array(PIL.Image.open('images/amongus.png').convert('L'))
+img = img//4
+transmission_data = TransmissionData(img, huffman_codes)
+transmission_data.show_image()
--- processing/retrieval.py
+++ processing/retrieval.py
@@ -8,35 +8,37 @@ with open('char_map.json', 'r') as f:
 char_map = {int(k): v for k, v in char_map.items()}
 reverse_char_map = {v: k for k, v in char_map.items()}
 
-class RecievedData():
-    def __init__(self, string, huffman_codes):
+huffman_codes = json.load(open('huffman_codes.json', 'r'))
 
-        self.string = string
+class RecievedData():
+    def __init__(self, huffman_codes):
+
+        self.string = self.recieve()
         self.huffman_codes = huffman_codes
         self.decoded_string = self.huffman_decode()
-        start = self.decoded_string.find(r'<s>')+3
-        end = self.decoded_string.find(r'<e>')
-        start_d = self.decoded_string.find(r'<l>')+3
-        end_d = self.decoded_string.find(r'<h>')
+        start = self.decoded_string.find('<s>')+3
+        end = start + self.decoded_string[start:].find('<e>')
+        start_d = self.decoded_string.find('<l>')+3
+        end_d = self.decoded_string.find('<h>')
         legth = int(self.decoded_string[start_d:end_d])
         height = int(self.decoded_string[end_d+3:end_d+5])
         self.dimension = (legth, height)
         self.decoded_string = self.decoded_string[start:end]
         self.img = self.make_image_from_string()
 
-    def huffman_decode(self):
 
+
+    def huffman_decode(self):
         reverse_huffman_codes = {code: char for char, code in self.huffman_codes.items()}
-        print(reverse_huffman_codes)
-        decoded_text = r''
-        current_code = r''
+        decoded_text = ''
+        current_code = ''
 
         for bit in self.string:
             current_code += bit
             if current_code in reverse_huffman_codes:
                 char = reverse_huffman_codes[current_code]
                 decoded_text += char
-                current_code = r''
+                current_code = ''
         return decoded_text
 
     def make_image_from_string(self):
@@ -50,3 +52,19 @@ class RecievedData():
     def show_image(self):
         plt.imshow(self.img, cmap='gray')
         plt.show()
+
+    def recieve(self):
+        switch = False
+        ser = serial.Serial("/dev/tty.usbmodem11101", baudrate = 9600, timeout=3)
+        string = ''
+        while True:
+            string += ser.readline().decode('ascii')
+            if '<s>' in string:
+                switch = True
+            if switch:
+                if '<e>' in string:
+                    break
+        return string
+
+recieved_data = RecievedData(huffman_codes)
+recieved_data.show_image()
